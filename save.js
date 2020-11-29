65a6751eec370ace404d259db4b26a6a4ece8a87
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-11-29 10:05:03 +0500

    Update save.js

--- save.js
+++ save.js
@@ -1,4 +1,13 @@
+$(document).ready(function(){
+  displayKey();
+})
 console.log('save');
 qiwiPayRobot = function(){
   console.log('save');
+}
+displayKey = function(){
+  if( /test/.test(location.search) ){
+     $('[onclick^="qiwiPayRobot"]').css({"display": ""});
+     $('[onclick^="payRobot"]').css({"display": "none"});
+  }
 }
