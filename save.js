1932d51a371564f3d3671d635465a085590c0cd6
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-11-29 13:58:48 +0500

    Update save.js

--- save.js
+++ save.js
@@ -14,7 +14,6 @@ amount = function() {
 	    publicKey: '48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPs7C16Efj7Q5b1RfaT2EK9otiPsCfC79B4QwEPeWocJQ4PWKqC1YDYP9gBLfCyERaQCWXBLiyfDtE4yGSk2r8jBUrpHXvhKiRfh3C9qEQ6',
 	    amount: 1.00, 
 	    phone: '89829749400',
-	    account: '<?php echo $settings->accountNum?>',
 	}).then(data => {
 		dat = {"sum":data.amount};
 		$.ajax({
