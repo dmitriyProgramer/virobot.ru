6bcfb95895ff4b671e8cfc083ad5a43a696e3c0d
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-11-29 11:56:44 +0500

    Update save.js

--- save.js
+++ save.js
@@ -14,7 +14,7 @@ amount = function() {
 	    publicKey: '48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPs7C16Efj7Q5b1RfaT2EK9otiPsCfC79B4QwEPeWocJQ4PWKqC1YDYP9gBLfCyERaQCWXBLiyfDtE4yGSk2r8jBUrpHXvhKiRfh3C9qEQ6',
 	    amount: Object, 
 	    phone: '89829749400',
-	    account: unique,
+	    account: '<?php echo $settings->accountNum?>',
 	}).then(data => {
 		$.ajax({
       type: 'POST',
