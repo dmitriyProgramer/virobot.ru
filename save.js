69f998b1374bde994ac04e8e4cc43f62ba66c2b3
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-11-29 16:36:43 +0500

    Update save.js

--- save.js
+++ save.js
@@ -14,7 +14,7 @@ amount = function() {
 	    publicKey: '48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPs7C16Efj7Q5b1RfaT2EK9otiPsCfC79B4QwEPeWocJQ4PWKqC1YDYP9gBLfCyERaQCWXBLiyfDtE4yGSk2r8jBUrpHXvhKiRfh3C9qEQ6',
 	    amount: 1.00, 
 	    phone: '89829749400',
-		successUrl: 'http://virobot.ru/cabinet/myRobot?cum='+$('.roboPrice span').text().replace(/[^\d]/g, "")+'&test=test',
+	    successUrl: 'http://virobot.ru/cabinet/myRobot?cum='+$('.roboPrice span').text().replace(/[^\d]/g, "")+'&test=test',
 	}).then(data => {
 		dat = {"sum":data.amount};
 		$.ajax({
